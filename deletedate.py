0d20f2ae0b5259615e26cadabce14f0febaf02ec
cn117 <niuchao8726@126.com>
2013-06-25 16:44:45 +0800

    config_add

--- deletedate.py
+++ deletedate.py
@@ -2,6 +2,7 @@
 import ConfigParser
 import math
 import MySQLdb
+import time
 class deletedate:
 	config_name=""
 	host_list=[]
@@ -9,11 +10,14 @@ class deletedate:
 	passwd_list=[]
 	db_list=[]
 	table_list=[]
+	column_list=[]
+	column_values_list=[]
+	limit_count_list=[]
 	def __init__(self,config_name):
 		if config_name==None:
 			print "The path is none!"
 		else:
-			print "deletedate begin!"
+			print "#####"+time.strftime('%Y-%m-%d-%H-%M-%S',time.localtime(time.time()))+"deletedate begin!######"
 			self.config_name=config_name
 	def readconfig(self):
 		config=ConfigParser.ConfigParser()
@@ -27,17 +31,32 @@ class deletedate:
 			self.passwd_list.append(config.get(i,"passwd"))
 			self.db_list.append(config.get(i,"db"))
 			self.table_list.append(config.get(i,"table"))
+			self.column_list.append(config.get(i,"column"))
+			self.column_values_list.append(config.get(i,"column_values"))
+			self.limit_count_list.append(config.get(i,"limit_count"))
 		for j in self.host_list:
 			print j
 	def deletedate(self):
 		num=0
 		for host in self.host_list:
-			print host+self.user_list[num]+self.passwd_list[num]+self.db_list[num]
+			print host+"###"+self.user_list[num]+"###"+self.passwd_list[num]+"###"+self.db_list[num]+"###"+self.table_list[num]+"###"+self.column_list[num]+"###"+self.column_values_list[num]+"###"+self.limit_count_list[num]
 			try:	
 				con=MySQLdb.connect(host=host,user=self.user_list[num],passwd=self.passwd_list[num],db=self.db_list[num])
 			except Exception, e:
 				print e
 				sys.exit()
+			cursor=con.cursor()
+			sql="DELETE FROM {0} where {1}={2} limit {3}"
+			print sql
+			print sql.format(self.table_list[num],self.column_list[num],self.column_values_list[num],self.limit_count_list[num])
+			try:
+				n=cursor.execute(sql.format(self.table_list[num],self.column_list[num],self.column_values_list[num],self.limit_count_list[num]))
+				con.commit()
+				print "deletedate num is: "+str(n)
+			except Exception, e:
+				print e
+			cursor.close()
+			con.close()
 			num=num+1
 		
 
